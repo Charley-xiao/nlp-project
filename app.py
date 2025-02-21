e5c9e06edfed2e9ca20e30d36d7130215459cbb6
Charley Xiao <59205189+Charley-xiao@users.noreply.github.com>
2025-02-21 13:16:37 +0800

    Revert

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sys
 import nltk
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger_eng')
+import spacy
+spacy.cli.download("en_core_web_sm")
 import random
 import argparse
 from model import ClassifierBackbone
